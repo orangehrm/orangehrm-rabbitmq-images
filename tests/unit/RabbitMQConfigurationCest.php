25c00f6defeaa63f757d60af896a80a63063378d
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2021-11-05 10:56:35 +0530

    changed test container name

--- tests/unit/RabbitMQConfigurationCest.php
+++ tests/unit/RabbitMQConfigurationCest.php
@@ -10,14 +10,14 @@ class RabbitMQConfigurationCest
     public function verifyManagementPluginIsEnabledTest(UnitTester $I)
     {
         $I->wantTo("Veify the Management Plugin is installed");
-        $I->runShellCommand("docker exec dev_rabbitmq rabbitmq-plugins list -e"); 
+        $I->runShellCommand("docker exec rabbitmq_container rabbitmq-plugins list -e"); 
         $I->seeInShellOutput("rabbitmq_management");
     }
 
     public function verifyDelayedMessageExchangePluginIsEnabledTest(UnitTester $I)
     {
         $I->wantTo("Veify the Delayed Message Exchange Plugin is installed");
-        $I->runShellCommand("docker exec dev_rabbitmq rabbitmq-plugins list -e"); 
+        $I->runShellCommand("docker exec rabbitmq_container rabbitmq-plugins list -e"); 
         $I->seeInShellOutput("rabbitmq_delayed_message_exchange");
     }
 }
