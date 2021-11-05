dde222e3ee79253624aaca48e7bd0e3e82920433
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2021-11-05 09:51:51 +0530

    added test cases to verify OS version and RabbitMQ version

--- tests/unit/ContainerEnvironmentCest.php
+++ tests/unit/ContainerEnvironmentCest.php
@@ -7,7 +7,17 @@ class ContainerEnvironmentCest
     }
 
     // tests
-    public function tryToTest(UnitTester $I)
+    public function vefiryOSVersionTest (UnitTester $I)
     {
+        $I->wantTo("Veify the container os version - Ubuntu 20.04");
+        $I->runShellCommand("docker exec dev_rabbitmq cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f 2"); 
+        $I->seeInShellOutput("Ubuntu 20.04.3 LTS");
+    }
+
+    public function vefiryInstalledRabbitMQVersionTest (UnitTester $I)
+    {
+        $I->wantTo("Veify the RabbitMQ version - 3.8.2");
+        $I->runShellCommand("docker exec dev_rabbitmq rabbitmqctl --version"); 
+        $I->seeInShellOutput("3.8.2");
     }
 }
