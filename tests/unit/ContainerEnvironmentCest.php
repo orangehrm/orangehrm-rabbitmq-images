88388ac6e46f143998cb3fc6a6b7aca3693f3c28
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2021-11-09 13:41:43 +0530

    test cases updated

--- tests/unit/ContainerEnvironmentCest.php
+++ tests/unit/ContainerEnvironmentCest.php
@@ -9,15 +9,15 @@ class ContainerEnvironmentCest
     // tests
     public function vefiryOSVersionTest (UnitTester $I)
     {
-        $I->wantTo("Veify the container os version - Ubuntu 20.04");
+        $I->wantTo("Veify the container os version - RHEL 8.4");
         $I->runShellCommand("docker exec rabbitmq_container cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f 2"); 
-        $I->seeInShellOutput("Ubuntu 20.04.3 LTS");
+        $I->seeInShellOutput("Red Hat Enterprise Linux 8.4 (Ootpa)");
     }
 
     public function vefiryInstalledRabbitMQVersionTest (UnitTester $I)
     {
-        $I->wantTo("Veify the RabbitMQ version - 3.8.2");
+        $I->wantTo("Veify the RabbitMQ version - 3.9.8");
         $I->runShellCommand("docker exec rabbitmq_container rabbitmqctl --version"); 
-        $I->seeInShellOutput("3.8.2");
+        $I->seeInShellOutput("3.9.8");
     }
 }
